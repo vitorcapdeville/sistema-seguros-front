d3048d4bd69722ec75fbb4a2158fc34c354eeff7
vitorcapdeville <vgcapdeville@hotmail.com>
2023-11-05 08:36:33 -0300

    convertendo produtos para lista para atender requisito do mvp

--- src/routes/produtos.element.jsx
+++ src/routes/produtos.element.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Produto = ({ id, nome, descricao }) => {
   return (
     <Link to={`simular/${id}`}>
-      <div className="w-64 p-2 border hover:border-gray-900 border-gray-300 text-left m-2 text-sm flex flex-col justify-between h-full rounded-md scale-90 hover:scale-100 transition duration-500 ease-in-out">
+      <li className="w-64 p-2 border hover:border-gray-900 border-gray-300 text-left m-2 text-sm flex flex-col justify-between h-full rounded-md scale-90 hover:scale-100 transition duration-500 ease-in-out">
         <h2 className="text-center text-lg font-bold">{nome}</h2>
         <p>{descricao}</p>
-      </div>
+      </li>
     </Link>
   );
 };
@@ -22,7 +22,7 @@ export default function Produtos({ produtos }) {
       <h2 className="text-2xl font-bold text-center mb-4">
         Escolha o seu produto
       </h2>
-      <div className="mt-5 grid grid-cols-3">
+      <ul className="mt-5 grid grid-cols-3">
         {produtos.map((produto) => (
           <Produto
             key={produto.id}
@@ -31,7 +31,7 @@ export default function Produtos({ produtos }) {
             descricao={produto.descricao}
           />
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
