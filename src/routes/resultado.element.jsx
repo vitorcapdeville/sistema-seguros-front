d173075c07ca75a3e73dc014f021978df8a198af
vitorcapdeville <vgcapdeville@hotmail.com>
2023-11-05 18:08:51 -0300

    Inclusão de botão para editar o beneficio Chamada da API movida para um único useEffect ao inves de chamar nos handle's API só é chamada ao confirmar a mudança de benefício.

--- src/routes/resultado.element.jsx
+++ src/routes/resultado.element.jsx
@@ -1,7 +1,9 @@
 import { Form, useLoaderData } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navegar from "./navegar.element";
 import { pegarSimulacao } from "../produtos";
+import { AiTwotoneEdit, AiOutlineCheckCircle } from "react-icons/ai";
+import Cleave from "cleave.js/react";
 
 const brl_formatter = new Intl.NumberFormat("pt-BR", {
   style: "currency",
@@ -10,12 +12,10 @@ const brl_formatter = new Intl.NumberFormat("pt-BR", {
 
 function PrazoRendaInput({ prazosRenda, value, onChange }) {
   return (
-    <>
-      <label htmlFor="prazoRenda" className="label">
-        Alterar prazo da renda
-      </label>
+    <div className="flex mx-auto">
+      <p className="text-center w-56 text-xl mt-2 mr-2">Prazo da renda:</p>
       <select
-        className="input focus:outline-none focus:bg-white"
+        className="input w-80 focus:outline-none focus:bg-white"
         id="prazoRenda"
         name="prazoRenda"
         onChange={onChange}
@@ -31,18 +31,16 @@ function PrazoRendaInput({ prazosRenda, value, onChange }) {
           </option>
         ))}
       </select>
-    </>
+    </div>
   );
 }
 
 function PrazoInput({ prazos, value, onChange }) {
   return (
-    <>
-      <label htmlFor="prazo" className="label">
-        Alterar prazo do produto
-      </label>
+    <div className="flex mx-auto items-center">
+      <p className="text-center w-56 text-xl mt-2 mr-2">Prazo:</p>
       <select
-        className="input focus:outline-none focus:bg-white"
+        className="input w-80 focus:outline-none focus:bg-white"
         id="prazo"
         name="prazo"
         onChange={onChange}
@@ -54,27 +52,46 @@ function PrazoInput({ prazos, value, onChange }) {
           </option>
         ))}
       </select>
-    </>
+    </div>
   );
 }
 
-function BeneficioInput({ value, onChange }) {
+function BeneficioInput({ value, edit, onEditSave, onChange }) {
   return (
-    <>
-      <label htmlFor="beneficio" className="label">
-        Alterar benefício
-      </label>
-      <input
-        id="beneficio"
-        type="range"
-        min="5000"
-        max="50000"
-        step="1000"
-        value={value}
-        onChange={onChange}
-        className="input"
-      />
-    </>
+    <div className="flex mx-auto items-center">
+      <p className="text-center w-56 text-xl mt-2 mr-2">Beneficio:</p>
+
+      <div className="relative rounded-md shadow-sm w-72">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+          <span className="text-gray-500">R$</span>
+        </div>
+        <Cleave
+          id="beneficio"
+          className={`input pl-8 ${
+            !edit ? "cursor-not-allowed bg-gray-100" : ""
+          }`}
+          name="beneficio"
+          value={value}
+          onChange={onChange}
+          disabled={!edit}
+          options={{
+            numeral: true,
+            numeralDecimalMark: ",",
+            delimiter: ".",
+          }}
+        />
+      </div>
+      {edit ? (
+        <button className="ml-3" onClick={onEditSave}>
+          {" "}
+          <AiOutlineCheckCircle />{" "}
+        </button>
+      ) : (
+        <button className="ml-3" onClick={onEditSave}>
+          <AiTwotoneEdit />
+        </button>
+      )}
+    </div>
   );
 }
 
@@ -86,45 +103,50 @@ export default function Resultado() {
   );
   const [premio, setPremio] = useState(parametrosSimulacao.premio);
   const [beneficioValor, setBeneficioValor] = useState(5000);
+  const [tempValor, setTempValor] = useState(beneficioValor);
+  const [editandoBeneficio, setEditandoBeneficio] = useState(false);
 
-  const handleChangeBeneficio = async (event) => {
-    setBeneficioValor(event.target.value);
-    let simulacao = await pegarSimulacao(
+  useEffect(() => {
+    pegarSimulacao(
       parametrosSimulacao.dataNascimento,
       parametrosSimulacao.sexo,
       prazo,
       prazoRenda,
       parametrosSimulacao.produtoId,
-      event.target.value
-    );
-    setPremio(simulacao.premio);
+      beneficioValor
+    ).then((simulacao) => {
+      setPremio(simulacao.premio);
+    });
+  }, [
+    prazo,
+    prazoRenda,
+    beneficioValor,
+    parametrosSimulacao.dataNascimento,
+    parametrosSimulacao.sexo,
+    parametrosSimulacao.produtoId,
+  ]);
+
+  const handleEditBeneficio = async (event) => {
+    event.preventDefault();
+    if (editandoBeneficio) {
+      setBeneficioValor(tempValor);
+    } else {
+      setTempValor(beneficioValor);
+    }
+    setEditandoBeneficio(!editandoBeneficio);
+  };
+
+  const handleBeneficioChange = (event) => {
+    setTempValor(event.target.rawValue);
   };
 
   const handleChangePrazo = async (e) => {
     setPrazo(e.target.value);
-    let simulacao = await pegarSimulacao(
-      parametrosSimulacao.dataNascimento,
-      parametrosSimulacao.sexo,
-      e.target.value,
-      prazoRenda,
-      parametrosSimulacao.produtoId,
-      beneficioValor
-    );
-    setPremio(simulacao.premio);
   };
 
   const handleChangePrazoRenda = async (e) => {
     let prazoRendaObj = JSON.parse(e.target.value);
     setPrazoRenda(prazoRendaObj);
-    let simulacao = await pegarSimulacao(
-      parametrosSimulacao.dataNascimento,
-      parametrosSimulacao.sexo,
-      prazo,
-      prazoRendaObj,
-      parametrosSimulacao.produtoId,
-      beneficioValor
-    );
-    setPremio(simulacao.premio);
   };
 
   return (
@@ -133,36 +155,30 @@ export default function Resultado() {
         Personalize o seu produto
       </h2>
       <p className="text-center text-xl mb-4">
-        Prêmio: {brl_formatter.format(premio)}.
+        Prêmio: {brl_formatter.format(premio)}
       </p>
-      <p className="text-center text-xl mb-4">
-        Beneficio: {brl_formatter.format(beneficioValor)}
-      </p>
+      <hr className="w-full mb-4" />
       <Form method="post" className="flex flex-col h-full w-full">
-        <div className="flex space-x-4 mb-4">
-          <div className="flex-grow">
-            <BeneficioInput
-              value={beneficioValor}
-              onChange={handleChangeBeneficio}
-            />
-          </div>
-          <div className="flex-grow">
-            {parametrosSimulacao.prazos.length ? (
-              <PrazoInput
-                prazos={parametrosSimulacao.prazos}
-                value={prazo}
-                onChange={handleChangePrazo}
-              />
-            ) : null}
-            {parametrosSimulacao.prazosRenda.length ? (
-              <PrazoRendaInput
-                prazosRenda={parametrosSimulacao.prazosRenda}
-                value={prazoRenda}
-                onChange={handleChangePrazoRenda}
-              />
-            ) : null}
-          </div>
-        </div>
+        <BeneficioInput
+          value={beneficioValor}
+          onChange={handleBeneficioChange}
+          onEditSave={handleEditBeneficio}
+          edit={editandoBeneficio}
+        />
+        {parametrosSimulacao.prazos.length ? (
+          <PrazoInput
+            prazos={parametrosSimulacao.prazos}
+            value={prazo}
+            onChange={handleChangePrazo}
+          />
+        ) : null}
+        {parametrosSimulacao.prazosRenda.length ? (
+          <PrazoRendaInput
+            prazosRenda={parametrosSimulacao.prazosRenda}
+            value={prazoRenda}
+            onChange={handleChangePrazoRenda}
+          />
+        ) : null}
         <div className="flex-grow"></div>
         <Navegar />
       </Form>
