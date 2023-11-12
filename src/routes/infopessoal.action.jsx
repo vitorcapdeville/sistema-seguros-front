a5250659ee6c7f4c50721a0ba42c45998224ae5c
vitorcapdeville <vgcapdeville@hotmail.com>
2023-11-12 09:39:59 -0300

    ajuste na acao de gerar a confirmacao para evitar que sejam adicioandos parametros de busca duplicados

--- src/routes/infopessoal.action.jsx
+++ src/routes/infopessoal.action.jsx
@@ -4,15 +4,9 @@ export async function action({ request, params }) {
   const formData = await request.formData();
   const parametrosSimulacao = Object.fromEntries(formData);
   const url = new URL(request.url);
-  url.searchParams.append("cpf", parametrosSimulacao.cpf);
-  url.searchParams.append("nome", parametrosSimulacao.nome);
-  url.searchParams.append("email", parametrosSimulacao.email);
+  url.searchParams.set("cpf", parametrosSimulacao.cpf);
+  url.searchParams.set("nome", parametrosSimulacao.nome);
+  url.searchParams.set("email", parametrosSimulacao.email);
   console.log(url);
   return redirect(`/contratar/${params.produtoId}/confirmar` + url.search);
-  // try {
-  //   await registrarCliente(parametrosSimulacao);
-  // } catch (e) {
-  //   return redirect(`/contratar/falha`);
-  // }
-  // return redirect(`/contratar/sucesso`);
 }
